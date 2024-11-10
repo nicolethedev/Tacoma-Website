ae4e1e0a9c4389f47931f0842109e47787a0b45b
Nicole <nicolelogan@hotmail.com>
2024-11-10 15:37:04 -0800

    removing javascript element at the moment, taking up too much time and will return to it if I have the spare time

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,22 +1,32 @@
 // Define Variables
 
-const hamburger = document.querySelector('hamburger-menu');
-const close = document.querySelector('close-menu');
-const links = document.querySelector('nav-menu-links');
+const menu = document.querySelector('.nav-menu-links');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger-menu');
+const closeIcon = document.querySelector('.close-menu');
+const menuIcon = document.querySelector('.menuIcon');
 
-// Add Event Listeners
+// Add function
 
-hamburger.addEventListener('click', () => {
-  links.classList.toggle('open');
-});
-
-let hamburgerOpen = false;
-hamburger.addEventListener('click', () => {
-  if (!hamburgerOpen) {
-    hamburger.classList.add('open');
-    hamburgerOpen = true;
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    hamburger.style.display = 'block';
   } else {
-    hamburger.classList.remove('open');
-    hamburgerOpen = false;
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    hamburger.style.display = 'none';
   }
-});
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+
+menuItems.forEach(
+  function (menuItem) {
+    menuItem.addEventListener('click', toggleMenu);
+  }
+);
+
+// When the user taps on hamburger, the menu will open and the hamburger icon will change to an X. When the user taps on the X, the menu will close and the X will change back to a hamburger icon.
